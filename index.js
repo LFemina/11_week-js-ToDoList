de80a86486aa64bdd960ec82ce0af9363efe7259
lfemina <lfemina@yandex.ru>
2024-07-19 09:26:45 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const taskInput = document.getElementById('taskInput');
+        const addButton = document.getElementById('addButton');
+        const taskList = document.getElementById('taskList');
+
+        addButton.addEventListener('click', function () {
+            const taskText = taskInput.value;
+            const newTask = document.createElement('li');
+            newTask.textContent = taskText;
+            taskList.append(newTask);
+            taskInput.value = ' ';
+        });
+
+        taskList.addEventListener('click', function (evt) {
+            if (evt.target.tagName === 'LI') {
+                evt.target.classList.toggle('completed');
+            }
+        });
